3a2549638ab7d5ea50943958d9627fe31ea74887
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-02-06 15:44:06 +0200

    Add closure

---
+++ assets/js/sandbox.js
@@ -0,0 +1,45 @@
+"use strict";
+
+let count = 100;
+// function counter(){
+//   count++;
+//   return count;
+// }
+
+// function createCounter(initialValue = 0) {
+//   let count = initialValue; //замкнута змінна
+//   return function (){
+//     count++;
+//     return count;
+//   }
+// }
+
+// const counter1 = createCounter();
+// const counter2 = createCounter(20);
+// const counter3 = createCounter(50);
+
+// function createCounter(initialValue = 0) {
+//   let count = initialValue;
+//   return {
+//     increment() {
+//       return ++count;
+//     },
+//     decrement() {
+//       return --count;
+//     },
+//   };
+// }
+// const counter1 = createCounter();
+// const counter2 = createCounter(200);
+// counter1.increment()
+
+// function createAdder(initialValue) {
+//   ///let result = initialValue;
+//   return function (value) {
+//     return (initialValue += value);
+//   };
+// }
+
+// const createAdder = (initital) => (value) => (initital += value);
+// const adder = createAdder(100);
+// adder(15);
