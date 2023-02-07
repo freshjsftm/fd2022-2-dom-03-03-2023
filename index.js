2715ae058c460890a2217589433d08b4404b9625
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-02-07 15:00:40 +0200

    Add createAttribute

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 "use strict";
 
-function handlerBtnUniq(event) {
-  // event.cancelBubble = true;
-  event.stopPropagation();
-  console.group();
-  //елемент на якому згенерувалася подія
-  console.log('target',event.target);
-  //елемент чий обробник виконується
-  console.log('currentTarget',event.currentTarget);
-  console.groupEnd();
-}
-
 const uniq = document.getElementById("uniq");
-uniq.addEventListener("click", handlerBtnUniq);
-window.addEventListener("click", handlerBtnUniq, true);
-document.addEventListener("click", handlerBtnUniq);
-document.body.addEventListener("click", handlerBtnUniq);
+
+const attrTitle = document.createAttribute("title");
+attrTitle.value = "button uniq";
+
+console.log(attrTitle)
+
+uniq.setAttributeNode(attrTitle)
+
+// uniq.setAttribute('title', 'qweqwe')
+// uniq.setAttribute('title', '11111')
