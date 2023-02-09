a3c17fea1db66e37a4c261bed189cc7e90c6ef9e
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-02-09 15:13:03 +0200

    Add event Submit & Reset

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 "use strict";
 
+const submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener('click', (event)=>{
+  //console.log(event.type)
+})
+
 const form = document.getElementById("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {target:{login}} = event;
+  console.dir(login.value);
+});
+// form.addEventListener("reset", (event) => {
+//   event.preventDefault();
+//   console.dir(event);
+// });
+
