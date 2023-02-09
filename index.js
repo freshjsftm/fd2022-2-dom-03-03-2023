998d6fc64faed1f38f3a1557d54e340e534c41e9
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-02-09 17:02:00 +0200

    Add createLiLogin

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 "use strict";
-
-const submitBtn = document.getElementById("submitBtn");
-submitBtn.addEventListener('click', (event)=>{
-  //console.log(event.type)
-})
-
+const logins = [];
+const list = document.getElementById("list"); //render
 const form = document.getElementById("form");
+const login = form.login;
+login.focus();
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const {target:{login}} = event;
-  console.dir(login.value);
+  const {
+    target,
+    target: { login },
+  } = event;
+  const newLogin = login.value.trim();
+  if (newLogin && logins.includes(newLogin) === false) {
+    logins.push(newLogin);
+    list.append(createLiLogin(newLogin));
+  }
+  target.reset();
 });
-// form.addEventListener("reset", (event) => {
-//   event.preventDefault();
-//   console.dir(event);
-// });
 
+function createLiLogin(newLogin) {
+  return createElement(
+    "li",
+    {},
+    createElement("span", {}, newLogin),
+    createElement(
+      "button",
+      {
+        styles: { color: "red", fontWeight: "bolder" },
+        listeners: {
+          click: ({ target: { parentElement, previousElementSibling } }) => {
+            parentElement.remove();
+            logins.splice(logins.indexOf(previousElementSibling.innerText), 1);
+          },
+        },
+      },
+      "X"
+    )
+  );
+}
