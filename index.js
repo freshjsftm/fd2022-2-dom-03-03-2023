cd9f70460830d7f900e2be45780f0a3596dbe0b8
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-02-14 16:22:06 +0200

    Add fetch & then

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 "use strict";
 
+fetch("./assets/js/data.json")
+  .then((response)=>{return response.json()})
+  .then((data)=>{console.table(data)})
 
+//const promise = fetch("./assets/js/data.json");
 
+// promise.then(
+//   (response) => {
+//     const responsePromise = response.json();
+//     responsePromise.then(
+//       (data) => {
+//         console.table(data);
+//       },
+//       () => {
+//         console.log("error responsePromise");
+//       }
+//     );
+//   },
+//   () => {
+//     console.log("error promise");
+//   }
+// );
+
+// const responsePromise = promise.then((response) => {
+//   return response.json();
+// });
+// responsePromise.then((data) => {
+//   console.table(data);
+// });
