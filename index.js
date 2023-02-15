73e4bd37b859a96981adda082f77a0a33deaf64b
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-02-15 14:48:10 +0200

    Add loadImage

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 "use strict";
 
-// setTimeout(callback, 100)
-// timeout(200).then(callback)
+const promise = new Promise((resolve, reject) => {
+  Math.random() > 0.5 ? resolve("data") : reject("error");
+});
 
-function timeout(time){
-  return new Promise((resolve, reject)=>{
-    setTimeout(resolve, time);
+promise
+  .then((data) => {
+    console.log(data);
   })
+  .catch((error) => {
+    console.log(error);
+  });
+
+function timeout(time, word) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(word);
+    }, time);
+  });
 }
 
-timeout(1500)
-  .then(()=>{console.log('resolve')})
-  .catch(()=>{console.log('reject')})
-  .finally(()=>{console.log('finally')})
+timeout(1500, "wordqwewqe").then((data) => {
+  console.log(data);
+});
 
-  
+function loadImage(path) {
+  const newImage = new Image();
+  newImage.src = path;
+  return new Promise((resolve, reject) => {
+    newImage.addEventListener("load", () => {
+      resolve(newImage);
+    });
+    newImage.addEventListener("error", () => {
+      reject(new Error('path invalid'));
+    });
+  });
+}
+
+loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Paracas_National_Reserve%2C_Ica%2C_Peru-3April2011.jpg/1200px-Paracas_National_Reserve%2C_Ica%2C_Peru-3April2011.jpg')
+  .then((elem)=>{document.body.append(elem)})
+  .catch((error)=>console.log(error))
